e99f5e3ffe241161de74685f2e19525fdc401f04
flatironschool <danliu277@gmail.com>
2020-08-27 19:00:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+function findAdjacent(name, vertices, edges) {
+    let adjEdges = []
+    let adjVertices = []
+    edges.each((edge) => {
+        if(edge.includes(name)) {
+            adjEdge = edge.find((adjName) => adjName !== name)
+            adjEdges.push(adjEdge)
+        }
+    })
+    adjEdges.each((edge) => {
+        let adjVertice = vertices.find((vertice) => vertice.name === name)
+        adjVertices.push(adjVertice)
+    })
+    return adjVertices
+}
