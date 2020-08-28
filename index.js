b74784b375b24b255730402f1c9b6cbe67e28b25
flatironschool <danliu277@gmail.com>
2020-08-28 15:59:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
-function bfs(rootNode, vertices, edges){
-
+function bfs(node, vertices, edges) {
+    node.distance = 0
+    let discovered = [node]
+    let discoverOrder = [node]
+    while (discovered.length != 0) {
+        let currentNode = discovered.shift()
+        let adjacentNodes = findAdjacent(currentNode.name, vertices, edges)
+        discoverOrder = discoverOrder.concat(adjacentNodes);
+        markDistanceAndPredecessor(currentNode, adjacentNodes)
+        discovered = discovered.concat(adjacentNodes)
+    }
+    return discoverOrder
 }
 
+// need to write a test for non-discovered nodes
 function findAdjacent(name, vertices, edges) {
-    let adjEdges = []
-    let adjVertices = []
-    edges.each((edge) => {
-        if(edge.includes(name)) {
-            adjEdge = edge.find((adjName) => adjName !== name)
-            adjEdges.push(adjEdge)
-        }
+    let adjacent = edges.filter((edge) => {
+        return edge.includes(name)
     })
-    adjEdges.each((edge) => {
-        let adjVertice = vertices.find((vertice) => vertice.name === name)
-        adjVertices.push(adjVertice)
+    adjacent = adjacent.map((edge) => {
+        return edge.find((node) => {
+            return node !== name
+        })
     })
-    return adjVertices
+    adjacent = adjacent.map((nodeName) => {
+        return vertices.find((vertex) => {
+            return vertex.name === nodeName
+        })
+    })
+    return adjacent.filter((node) => {
+        return node.distance == null;
+    })
+}
+
+function markDistanceAndPredecessor(node, adjacentNodes) {
+    adjacentNodes.map((adjNode) => {
+        adjNode.distance = node.distance + 1;
+        adjNode.predecessor = node;
+    })
 }
